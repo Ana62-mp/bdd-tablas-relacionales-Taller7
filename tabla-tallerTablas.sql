c15b9cf5d4076e5444297379f66c95f738f9ee11
Ana <76455167+Ana62-mp@users.noreply.github.com>
2026-05-23 22:05:10 -0500

    APMP: tabla de rompimiento libro_autor

--- tabla-tallerTablas.sql
+++ tabla-tallerTablas.sql
@@ -18,4 +18,18 @@ CREATE TABLE autores(
 	pais varchar(30) NOT NULL
 );
 
+-- PARTE 3: Tabla de Rompimiento
+
+CREATE TABLE libro_autor(
+	la_libro_codigo_fk varchar(5) NOT NULL,
+	la_autor_id_fk int NOT NULL,
+	anio_publicacion int NOT NULL,
+
+	--PRIMARY KEY DE LA TABLA
+	CONSTRAINT pk_libro_autor PRIMARY KEY (la_libro_codigo_fk, la_autor_id_fk),
+
+	--FOREIGN KEY DE LA TABLA
+	CONSTRAINT  fk_libro FOREIGN KEY(la_libro_codigo_fk) REFERENCES libros(codigo),
+	CONSTRAINT  fk_autor FOREIGN KEY(la_autor_id_fk) REFERENCES autores(id)
+);
 
