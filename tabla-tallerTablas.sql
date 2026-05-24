e642a2576b6b03628c0d736c458397997d886267
Ana <76455167+Ana62-mp@users.noreply.github.com>
2026-05-23 22:04:33 -0500

    APMP: Crear tablas libros y autores
    Added table creation for 'libros' and 'autores' in the database.

--- tabla-tallerTablas.sql
+++ tabla-tallerTablas.sql
@@ -1,5 +1,21 @@
 -- PARTE 1: Creación base de datos
 CREATE DATABASE biblioteca_db;
 
+--PARTE2: Crear la tabla libros y tabla autores
+
+DROP TABLE IF EXISTS libros;
+DROP TABLE IF EXISTS autores;
+
+CREATE TABLE libros(
+	codigo varchar(5) PRIMARY KEY,
+	titulo varchar(50) NOT NULL,
+	paginas int NOT NULL
+);
+
+CREATE TABLE autores(
+	id int PRIMARY KEY,
+	nombre varchar(50) NOT NULL,
+	pais varchar(30) NOT NULL
+);
 
 
